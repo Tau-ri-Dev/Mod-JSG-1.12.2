0663ce1d7a9cf3c775293d816d3108711444d5f3
MineDragonCZ <78699120+MineDragonCZ@users.noreply.github.com>
2022-02-20 10:47:37 +0100

    Create wiki.js

---
+++ wiki.js
@@ -0,0 +1,73 @@
+// ENGLISH TRANSLATIONS
+var en = {
+	_navigation:{
+		home:"Home",
+		features:"Features",
+		wiki:"Wiki",
+		team:"Team",
+		minecraft_servers:"Servers",
+		aunisworlds:"Aunis Worlds",
+		auniscraft:"Aunis Craft",
+	},
+	_footer:{
+		main:"The Aunis mod",
+		desc:"The first realistic stargate mod in minecraft!",
+		author:"Created by:",
+		changelog:"Changelog",
+		other_projects:"Other projects:",
+	},
+	_groups:{
+		g_helper:"Helper",
+		g_developer:"Developer",
+		g_discordadmin:"Discord Admin",
+		g_translator:"Translator",
+		g_rescreator:"Resources Creator",
+		g_tester:"Prealpha tester",
+	},
+	404:{
+		doc_title:"404",
+		page_title:"404",
+		page_subtitle:"Not found",
+
+		main:"Target site was not found!",
+	},
+	403:{
+		doc_title:"403",
+		page_title:"403",
+		page_subtitle:"Forbidden",
+
+		main:"No permissions to view this directory!",
+	},
+
+	home:{
+		doc_title:"Home",
+		page_title:"Aunis",
+		page_subtitle:"All you need is stargate...",
+
+		who:"What is Aunis?",
+		who_desc:`
+			<li> Aunis is a mod, with the theme of the famous TV series Stargate. </li>
+			<li> This mod adds devices like Stargates and Transport Rings to the game. </li>
+			<li> You'll also find a variety of materials for making the devices already mentioned. </li>
+			<li> For example: Trinium, Titanium, Naquadah and more. </li>
+			<li> This mod also includes Irises in three variants, so you can secure your base from uninvited guests. </li>
+		`,
+		how:"How Aunis works?",
+		how_desc:`
+			<li> Aunis is a mod, with the theme of the famous TV series Stargate. </li>
+			<li> This mod adds devices like Stargates and Transport Rings to the game. </li>
+			<li> You'll also find a variety of materials for making the devices already mentioned. </li>
+			<li> For example: Trinium, Titanium, Naquadah and more. </li>
+			<li> This mod also includes Irises in three variants, so you can secure your base from uninvited guests. </li>
+		`,
+	},
+	team:{
+		doc_title:"Our Team",
+		page_title:"Our Team",
+		page_subtitle:"",
+
+		developers:"Development",
+		helpers:"Helper team",
+		other:"Other groups",
+	},
+}
