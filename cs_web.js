c729fa5900d6fb447e59f7ec283cfa1c2c8a263b
MineDragonCZ <78699120+MineDragonCZ@users.noreply.github.com>
2022-02-22 20:23:05 +0100

    Create cs_web.js

---
+++ cs_web.js
@@ -0,0 +1,121 @@
+// CZECH TRANSLATIONS
+var cs = {
+	_navigation:{
+		home:"Domů",
+		features:"Vlastnosti",
+		wiki:"Wiki",
+		team:"Tým",
+		minecraft_servers:"Servery",
+		aunisworlds:"Aunis Worlds",
+		auniscraft:"Aunis Craft",
+
+		login:"Přihlásit se",
+
+		csMark:"✔",
+		enMark:"",
+		frMark:"",
+
+		logout:"Odhlásit se",
+		dashboard:"Panel",
+	},
+	_footer:{
+		main:"The Aunis mod",
+		desc:"První realistický stargate mod do minecraftu!",
+		author:"Vytvořil:",
+		changelog:"Seznam změn",
+		other_projects:"Další projekty:",
+	},
+	_groups:{
+		g_founder:"Zakladatel",
+		g_helper:"Pomocník",
+		g_developer:"Vývojář",
+		g_discordadmin:"Discord Admin",
+		g_translator:"Překladátor",
+		g_rescreator:"Tvůrce modelů",
+		g_tester:"Prealpha Tester",
+	},
+	404:{
+		doc_title:"404",
+		page_title:"404",
+		page_subtitle:"Not found",
+
+		main:"Požadovaná stránka nebyla nalezena!"
+	},
+	403:{
+		doc_title:"403",
+		page_title:"403",
+		page_subtitle:"Forbidden",
+
+		main:"Požadovaná složka je zabezpečena!"
+	},
+	changelog:{
+		doc_title:"Seznam změn",
+		page_title:"Změny",
+		page_subtitle:"Seznam změn",
+
+		version:"Aktuální verze: "
+	},
+
+	home:{
+		doc_title:"Domů",
+		page_title:"Aunis",
+		page_subtitle:"All you need is stargate...",
+
+		who:"Co je to Aunis?",
+		who_desc:`
+			<li>Aunis je mód, s tématickou známého televizního seriálu Hvězdná brána.</li>
+			<li>Tento mód přidává do hry zařízení jako Hvězdnou Bránu a Transportní kruhy.</li>
+			<li>Dále zde naleznete spustu materiálů pro výrobu již zmíněných zařízení.</li> 
+			<li>Například: Trinium, Titanium, Naquadah a mnoho dalšího.</li>
+			<li>Tento mód rovněž obsahuje Iris a to ve třech variantách, takže si můžete zabezpečit svůj dům před nezvanými hosty.</li>
+		`,
+		how:"Jak Aunis funguje?",
+		how_desc:`
+			<li>Aunis Vás pomocí hvězdné brány dokáže dopravit kdekoliv na světě, a i dokonce do jiných dimenzí!</li>
+			<li>transportní kruhy fungují na malé vzdálenosti, něco jako výtah.</li>
+			<li>Podrobný tutoriál naleznete zde:</li>
+		`,
+		how_btn:'Otevřít tutoriál',
+	},
+	team:{
+		doc_title:"Náš Tým",
+		page_title:"Náš Tým",
+		page_subtitle:"",
+
+		developers:"Vývojáři",
+		helpers:"Výpomoc",
+		other:"Ostatní skupiny",
+	},
+	login:{
+		doc_title:"Přihlášení",
+		page_title:"",
+		page_subtitle:"",
+
+		login:"Přihlašte se",
+		user:"Jméno",
+		password:"Heslo",
+		loginBtn:"Přihlásit se",
+
+		"2fa":`
+			Na tomto účtu je zapnuto dvoufázové ověření! Prosíme, zadejte svůj ověřovací kód z aplikace.
+		`,
+		code:"Kód",
+
+		// alerts nand states
+		wrongCode:'Neplatný kód',
+		wrongLogin:'Neplatný login',
+		loggedSuccess:'Úspěšně přihlášen',
+		missingCaptcha:'Prosím, vyplňte recaptchu',
+		unknownError:'Neznámá chyba!',
+		missingParameter:'Prosím, vyplňte všechna pole',
+	},
+	dashboard:{
+		doc_title:"Panel",
+		page_title:"Panel",
+		page_subtitle:"Administrace účtu",
+	},
+	dashboard_nav:{
+		settings:"Nastavení",
+		informations:"Informace",
+	},
+}
