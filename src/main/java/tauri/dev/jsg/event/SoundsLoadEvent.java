edde6b950e2b085ae8d318088401e8910f07a6c4
MineDragonCZ <78699120+MineDragonCZ@users.noreply.github.com>
2023-05-02 23:03:02 +0200

    Fixed server-side crash

--- src/main/java/tauri/dev/jsg/event/SoundsLoadEvent.java
+++ src/main/java/tauri/dev/jsg/event/SoundsLoadEvent.java
@@ -4,6 +4,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import tauri.dev.jsg.JSG;
 import tauri.dev.jsg.config.JSGConfig;
 import tauri.dev.jsg.sound.SoundPositionedEnum;
@@ -13,12 +15,13 @@ import java.util.TimerTask;
 
 import static tauri.dev.jsg.sound.JSGSoundHelperClient.playPositionedSoundClientSide;
 
-@Mod.EventBusSubscriber
+@SideOnly(Side.CLIENT)
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class SoundsLoadEvent {
 
     @SubscribeEvent
     public static void onSoundHandlerLoad(SoundLoadEvent event) {
-        JSG.info("looool");
+        if(!JSGConfig.General.mainMenuConfig.loadingMusic) return;
         Timer t = new Timer();
         t.schedule(new TimerTask() {
             @Override
@@ -26,7 +29,6 @@ public class SoundsLoadEvent {
                 if (JSGConfig.General.mainMenuConfig.loadingMusic)
                     playPositionedSoundClientSide(new BlockPos(0, 0, 0), SoundPositionedEnum.LOADING_MUSIC, true);
                 t.cancel();
-                JSG.info("Run!");
             }
         }, 5000, 5000);
     }
