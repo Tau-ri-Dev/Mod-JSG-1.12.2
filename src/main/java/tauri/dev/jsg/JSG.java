646cb4cdc4e36c2671baeb4d3d413ce6538565f0
MineDragonCZ_ <minedragoncz@email.cz>
2023-07-07 22:56:58 +0200

    Version 4.11.1.0

--- src/main/java/tauri/dev/jsg/JSG.java
+++ src/main/java/tauri/dev/jsg/JSG.java
@@ -36,7 +36,7 @@ public class JSG {
     public static final String MOD_ID = "jsg";
     public static final String MOD_NAME = "Just Stargate Mod";
     public static final String MOD_VERSION = "@VERSION@";
-    public static final int DATA_VERSION = 26;
+    public static final int DATA_VERSION = 27;
     public static final String CONFIG_GENERAL_VERSION = "3.2";
     public static final String CONFIG_CRAFTINGS_VERSION = "1.2";
     public static final String CONFIG_STRUCTURES_VERSION = "2.0";
