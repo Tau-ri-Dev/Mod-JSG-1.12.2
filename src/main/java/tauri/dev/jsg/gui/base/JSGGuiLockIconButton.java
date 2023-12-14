77cbe48ecb5c607e7965e7908caf6484bbf56ccd
MineDragonCZ_ <minedragoncz@email.cz>
2023-12-14 18:06:35 +0100

    Removed unused classes

--- src/main/java/tauri/dev/jsg/gui/base/JSGGuiLockIconButton.java
+++
@@ -1,9 +0,0 @@
-package tauri.dev.jsg.gui.base;
-
-import net.minecraft.client.gui.GuiLockIconButton;
-
-public class JSGGuiLockIconButton extends GuiLockIconButton {
-    public JSGGuiLockIconButton(int buttonId, int x, int y) {
-        super(buttonId, x, y);
-    }
-}
